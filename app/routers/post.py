5d3a3c8a7e8e1e0148e2342ccae8897b9144910f
mikekaranja <mikekaranja63@gmail.com>
2022-07-31 12:47:02 +0300

    added: new changes 4;

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-from sqlalchemy.orm import Session
 from typing import List, Optional
 
 from sqlalchemy import func
-# from sqlalchemy.sql.functions import func
+from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
 from ..database import get_db
-
 
 router = APIRouter(
     prefix="/posts",
@@ -16,7 +14,8 @@ router = APIRouter(
 
 # @router.get("/", response_model=List[schemas.Post])
 @router.get("/", response_model=List[schemas.PostOut])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10,
+              skip: int = 0, search: Optional[str] = ""):
     # results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
     #     models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id)
 
@@ -33,12 +32,14 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     #     models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -60,7 +61,8 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
     # post = db.query(models.Post).filter(models.Post.id == id).first()
 
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
+        models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -71,7 +73,6 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
     # cursor.execute(
     #     """DELETE FROM posts WHERE id = %s returning *""", (str(id),))
     # deleted_post = cursor.fetchone()
@@ -95,8 +96,8 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
 
